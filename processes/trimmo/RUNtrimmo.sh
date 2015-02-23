d30acbf1c215ba6b578a447696fc623c0d42fc84
Marian Schubert <marian.schubert@nmbu.no>
2015-02-23 17:51:55 +0100

    removed $outfolder

--- processes/trimmo/RUNtrimmo.sh
+++ processes/trimmo/RUNtrimmo.sh
@@ -9,4 +9,4 @@ R2=$(find $infolder -name *R2_001.fastq.gz)
 
 java -jar /local/genome/packages/trimmomatic/0.32/trimmomatic-0.32.jar PE -threads 2 $R1 $R2 $sampleID.R1.fq $sampleID.R1.unpaired.fq $sampleID.R2.fq $sampleID.R2.unpaired.fq ILLUMINACLIP:TruSeq3-PE-2.fa:2:30:10 CROP:120 SLIDINGWINDOW:40:15 MINLEN:36 
 
-fastqc -t 2 -o fastqc_output $outfolder/$sampleID.R1.fq $outfolder/$sampleID.R2.fq
+fastqc -t 2 -o fastqc_output $sampleID.R1.fq $sampleID.R2.fq
