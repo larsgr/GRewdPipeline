0173c50c65c0239f45488247e1f17e3a1820eda2
Lars Gronvold <lars.gronvold@gmail.com>
2015-02-26 15:14:07 +0100

    Use ... to add additional sbatch parameters

--- processes/SLURMscript/createSLURMscript.R
+++ processes/SLURMscript/createSLURMscript.R
@@ -2,7 +2,26 @@ source("R/fillTemplateFile.R")
 
 # function that generates a SLURM script into which you can insert a script of your own
 
-createSLURMscript <- function(script,workdir,jobName,ntasks=1,nodes=1,SBATCHoptions=""){
+createSLURMscript <- function(script,workdir,jobName,ntasks=1,nodes=1,SBATCHoptions="",...){
+  
+  # pass additional parameters to sbatch
+  params <- list(...)
+  
+  for( i in seq_along(params)){
+    paramName <- gsub("_","-",names(params)[i])
+    paramValue <- params[[i]]
+    if(is.logical(paramValue)){
+      if(paramValue==TRUE)
+        SBATCHoptions <- paste(
+          paste0("#SBATCH --",paramName),
+          SBATCHoptions, sep="\n")
+    } else {
+      SBATCHoptions <- paste(
+        paste0("#SBATCH --",paramName,"=",paramValue),
+        SBATCHoptions, sep="\n")
+    }
+  }
+  
   txt <- fillTemplateFile(c(script=paste(script,collapse="\n"), workdir=workdir, ntasks=ntasks,
                             nodes=nodes,jobName=jobName,SBATCHoptions=SBATCHoptions),
                           templateFile="processes/SLURMscript/SLURMscript.template.sh")
